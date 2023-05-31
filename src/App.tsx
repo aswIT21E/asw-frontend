799856fcf12d7ce146ee951a40bd7142b33ab321
ruben.torrente <ruben.torrente@estudiantat.upc.edu>
2023-05-31 21:04:22 +0200

    info user y profile sin navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/home';
 import IssuePage from './pages/IssuePage/issuePage';
+import { Login } from './pages/Login/login';
+import { login } from './services/loginService';
+import { AddIssue } from './pages/CRUD/crud';
+import { BulkIssue } from './pages/CRUD/bulk';
+import { Profile } from './pages/Profile/profile';
 
-function App({ children }: { children: ReactNode }) {
+function App() {
+
+  const [authenticated, setAuthenticated] = useState(!!localStorage.getItem('token'));
   
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/issue/:id" Component={IssuePage} />
+          <Route path="/issue/:id" element={<IssuePage />} />
+          <Route
+            path="/"
+            element={
+              authenticated ? (
+                <Home />
+              ) : (
+                <Login login={(username, password) => login(username, password, setAuthenticated)} />
+              )
+            }
+          />
+          <Route path="/login" element={<Login login={(username, password) => login(username, password, setAuthenticated)} />} />
+          <Route path="/issues/newIssue" element={<AddIssue />} />
+          <Route path="/issues/bulk" element={<BulkIssue />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </BrowserRouter>
