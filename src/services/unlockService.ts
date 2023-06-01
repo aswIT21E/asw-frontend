3c369df36b17e949d4b430660b270855a21aa91e
Joel <jmaciasrojas@gmail.com>
2023-06-01 16:53:34 +0200

    lock unlock añadido

---
+++ src/services/unlockService.ts
@@ -0,0 +1,17 @@
+export const unlockIssue = async (id: string) => {
+    try {
+      const response = await fetch(`http://api.asw-fib.net/issues/${id}/unlock-issue`, {
+        headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVsc2EiLCJpYXQiOjE2ODUzNTMyNDd9.sOGIHXk-CTXi3t9Z5mHCEKXBo-_kOzljhmTgLqPOj0E',
+          'Content-Type': "application/x-www-form-urlencoded"
+        },
+        method: 'PUT',
+      });
+      const fetchedIssue = await response.json();
+      return fetchedIssue;
+    } catch (error) {
+      console.log('Error fetching issue:', error);
+      return null;
+    }
+  };
+  
